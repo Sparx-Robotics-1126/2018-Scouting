02335aa75cc3d847c8cea4ebdb386422f420f9d6
daleihuang <dalhuang@yahoo.com>
2018-01-22 20:01:51 -0500

    Fixing the scoutername text

--- app/src/main/java/steamworks/sparx1126/com/powerup/MainActivity.java
+++ app/src/main/java/steamworks/sparx1126/com/powerup/MainActivity.java
@@ -2,18 +2,26 @@ package steamworks.sparx1126.com.powerup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.text.TextWatcher;
 import android.text.Editable;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    String[] studentList = {"Felix", "Huang"};
+    AutoCompleteTextView scoutername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AutoCompleteTextView scoutername = findViewById(R.id.scoutername);
+        scoutername = findViewById(R.id.scoutername);
         scoutername.addTextChangedListener(scouterTextEntered);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, studentList);
+        scoutername.setAdapter(adapter);
+        scoutername.setThreshold(1);
     }
 
 
@@ -28,10 +36,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void afterTextChanged(Editable s) {
-            System.out.println(s.toString());
-           /* String scouterName = getScouterName();
-            if (Arrays.asList(studentList).contains(scouterName)) {
+        public void afterTextChanged(Editable lastInput) {
+            String scouterName = lastInput .toString();
+            scouterName = scouterName.replace("\\n", "");
+            scouterName = scouterName.replace("\\r", "");
+            System.out.println(scouterName);
+            if (Arrays.asList(studentList).contains(scoutername.getText().toString())) {
+                System.out.print("I got a real name");
+            }
+           /* if (Arrays.asList(studentList).contains(scouterName)) {
                 SharedPreferences.Editor editor = settings.edit();
                 editor.putString(getResources().getString(R.string.pref_scouter), scouterName);
                 editor.apply();
