545bc8456c158d3d537a5c544e7f4f868ba30c05
JCas127 <jarencascino@gmail.com>
2018-01-29 20:11:09 -0500

    29th benchmarking complete java started

---
+++ app/src/main/java/sparx1126/com/powerup/components/BenchmarkData.java
@@ -0,0 +1,18 @@
+package sparx1126.com.powerup.components;
+
+/**
+ * Created by jcascino20 on 1/29/2018.
+ */
+
+public class BenchmarkData {
+    public int getTeamnumber() {
+        return teamnumber;
+    }
+
+    public void setTeamnumber(int teamnumber) {
+        this.teamnumber = teamnumber;
+    }
+
+    int teamnumber;
+
+}
