9fee10faaaadcb05397685e8aa4f3bdc9dc36c23
Hiram Jimenez <jimenez.hiram@gmail.com>
2018-01-31 19:49:17 -0500

    Finished UI, and started writing java along w/ it.

--- app/src/main/java/sparx1126/com/powerup/Scouting.java
+++ app/src/main/java/sparx1126/com/powerup/Scouting.java
@@ -2,12 +2,66 @@ package sparx1126.com.powerup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import sparx1126.com.powerup.data_components.ScoutingData;
 
 public class Scouting extends AppCompatActivity {
+    private ScoutingData scoutingData;
+    private EditText teamnum;
+    private RadioButton blueAlliancecolor;
+    private RadioButton redAlliancecolor;
+    private EditText matchnum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scouting);
+
+         scoutingData = new ScoutingData();
+        teamnum = findViewById(R.id.scouteamnuminput);
+        teamnum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                scoutingData.setTeamnumber(Integer.parseInt(editable.toString()));
+                Log.e("afterTextChanged", Integer.toString(scoutingData.getTeamnumber()));
+            }
+        });
+    matchnum=findViewById(R.id.matchnumimput);
+    matchnum.addTextChangedListener(new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            scoutingData.setTeamnumber(Integer.parseInt(editable.toString()));
+            Log.e("afterTextChanged", Integer.toString(scoutingData.getTeamnumber()));
+
+        }
+    });
+    blueAlliancecolor = findViewById(R.id.blueAlliancebuttton);
+
     }
+
 }
