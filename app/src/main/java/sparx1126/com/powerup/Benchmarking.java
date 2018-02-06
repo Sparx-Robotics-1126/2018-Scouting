a185ede90ed7da5aff8cda4b3f8f7a5a3c1857e4
Hiram Jimenez <jimenez.hiram@gmail.com>
2018-02-05 20:08:35 -0500

    Merge branches 'Benchmark' and 'master' of https://github.com/Sparx-Robotics-1126/2018-Scouting into Benchmark
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/sparx1126/com/powerup/Benchmarking.java

--- app/src/main/java/sparx1126/com/powerup/Benchmarking.java
+++ app/src/main/java/sparx1126/com/powerup/Benchmarking.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 import sparx1126.com.powerup.data_components.BenchmarkData;
 
@@ -14,6 +16,27 @@ public class Benchmarking extends AppCompatActivity {
     private BenchmarkData currentData;
     private EditText teamnumber;
     private EditText speed;
+    private EditText height;
+    private EditText weight;
+    private EditText climbheight;
+    private CheckBox start_w_cube;
+    private CheckBox move_past_line;
+    private CheckBox auto_scale;
+    private CheckBox auto_switch;
+    private CheckBox acquire_floor;
+    private CheckBox get_from_portal;
+    private CheckBox deposit_vault;
+    private CheckBox score_switch;
+    private CheckBox score_scale;
+    private CheckBox climb_rung;
+    private CheckBox has_rungs;
+    private CheckBox attach_robot;
+    private RadioButton place_switch;
+    private RadioButton toss_switch;
+    private RadioButton place_scale;
+    private RadioButton toss_scale;
+    private RadioButton pref_floor;
+    private RadioButton pref_portal;
 
 
 
