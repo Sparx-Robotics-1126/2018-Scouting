4021001bdeb32816db0317c03242ce683dc6c6fa
Hiram Jimenez <jimenez.hiram@gmail.com>
2018-02-10 11:37:07 -0500

    fixing error in scouting

---
+++ app/src/main/java/sparx1126/com/powerup/Directory.java
@@ -0,0 +1,17 @@
+package sparx1126.com.powerup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hiram on 2/10/2018.
+ */
+
+public class Directory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.directory);
+
+    }
+}
--- app/src/main/java/sparx1126/com/powerup/MainActivity.java
+++ app/src/main/java/sparx1126/com/powerup/MainActivity.java
@@ -1,12 +1,15 @@
 package sparx1126.com.powerup;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 import java.util.Map;
 
@@ -18,12 +21,13 @@ import sparx1126.com.powerup.utilities.Logger;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity ";
-    private static final String[] studentList = {"Felix", "Huang"};
+    private String[] studentList;
     private static final int GOOGLE_REQUEST_CODE_SIGN_IN = 0;
     private static Logger logger;
     private static BlueAllianceNetworking blueAlliance;
     private static FileIO fileIO;
     private static GoogleDriveNetworking googleDrive;
+    private Button loginButton;
     private AutoCompleteTextView studentNameAutoTextView;
 
     @Override
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         blueAlliance = BlueAllianceNetworking.getInstance();
         fileIO = FileIO.getInstance(this);
         googleDrive = GoogleDriveNetworking.getInstance();
+
         // if failed auto sign then googleDrive will return an internt to try to
         // sign in by asking the user to select an account
         // This is done only once here in MainActivity
@@ -46,18 +51,32 @@ public class MainActivity extends AppCompatActivity {
         else {
             logger.Log(TAG, "Logged into Google Drive!", Logger.MSG_TYPE.NORMAL, this);
         }
+loginButton = findViewById(R.id.logInButton);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String studentName = studentNameAutoTextView.getText().toString();
+                        for(String student: studentList){
+                            if(student.equals(studentName)){
+                                Intent intent = new Intent(MainActivity.this, Directory.class);
+                                startActivity(intent);
+                            }
+                        }
+                                           }
+                                       });
         // student selection
+        studentList = getResources().getStringArray(R.array.students);
         studentNameAutoTextView = findViewById(R.id.studentNameAutoText);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, studentList);
         studentNameAutoTextView.setAdapter(adapter);
         studentNameAutoTextView.setThreshold(1);
-        studentNameAutoTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View arg1, int pos, long id) {
-                String selectedStudent = (String) parent.getItemAtPosition(pos);
-            }
-        });
+//        studentNameAutoTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View arg1, int pos, long id) {
+//                String selectedStudent = (String) parent.getItemAtPosition(pos);
+//            }
+//        });
 
         blueAlliance.downloadEventsSparxsIsIn(new BlueAllianceNetworking.CallbackEvents() {
             @Override
